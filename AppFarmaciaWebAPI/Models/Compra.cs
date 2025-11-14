6ed2831d2b8d219a7af7cb44f9aacf9aa63f1cb1
Mateo Centis <mateo.centis@hotmail.com.ar>
2025-11-13 21:32:37 -0300

    Te cojí la boca LUCAS

--- AppFarmaciaWebAPI/Models/Compra.cs
+++ AppFarmaciaWebAPI/Models/Compra.cs
@@ -7,8 +7,8 @@ public partial class Compra
 {
     public int IdCompra { get; set; }
     public DateTime Fecha { get; set; }
-    public string Proveedor { get; set; } = null!;
-    public string Descripcion { get; set; } = null!;
+    public string? Proveedor { get; set; } = null!;
+    public string? Descripcion { get; set; } = null!;
 
     public bool CompraConfirmada { get; set; }
 
